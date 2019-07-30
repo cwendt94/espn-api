1d0302e2e3d812548e1c0b8c2a9c0262b947191e
Christian Wendt <cwendt94@vt.edu>
2019-07-30 11:54:54 -0700

    Add pick object. Add league draft variable with a list of pick objects

--- ff_espn_api/matchup.py
+++ ff_espn_api/matchup.py
@@ -13,18 +13,3 @@ class Matchup(object):
         self.home_score = self.data['home']['totalPoints']
         self.away_team = self.data['away']['teamId']
         self.away_score = self.data['away']['totalPoints']
-        # if self.data['teams'][0]['home'] and not self.data['bye']:
-        #     self.home_team = self.data['teams'][0]['teamId']
-        #     self.home_score = self.data['teams'][0]['score']
-        #     self.away_team = self.data['teams'][1]['teamId']
-        #     self.away_score = self.data['teams'][1]['score']
-        # elif self.data['teams'][0]['home'] and not self.data['bye']:
-        #     self.home_team = self.data['teams'][1]['teamId']
-        #     self.home_score = self.data['teams'][1]['score']
-        #     self.away_team = self.data['teams'][0]['teamId']
-        #     self.away_score = self.data['teams'][0]['score']
-        # else:
-        #     self.home_team = self.data['teams'][0]['teamId']
-        #     self.home_score = self.data['teams'][0]['score']
-        #     self.away_team = None
-        #     self.away_score = None
