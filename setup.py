cb1263c1cb1c9a63b443d0752838e00ac35e5175
Christian Wendt <cwendt94@vt.edu>
2019-07-30 13:00:20 -0700

    Update package version 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ff_espn_api',
     packages=['ff_espn_api'],
-    version='1.0.1',
+    version='1.0.2',
     author='Christian Wendt',
     description='Fantasy Football ESPN API',
     install_requires=['requests>=2.0.0,<3.0.0'],
