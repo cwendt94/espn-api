042786bc48f353239810c9e64f55323c35a4b13a
Ben <bsmiller25@gmail.com>
2019-10-30 08:31:18 -0400

    basketball: matchup class no longer keeps full json data

--- espn_api/basketball/matchup.py
+++ espn_api/basketball/matchup.py
@@ -3,14 +3,13 @@ from .constant import STATS_MAP
 class Matchup(object):
     '''Creates Matchup instance'''
     def __init__(self, data):
-        self.data = data
-        self._fetch_matchup_info()
+        self._fetch_matchup_info(data)
 
     def __repr__(self):
         # TODO: use final score when that's available?
         # writing this too early to see if data['home']['totalPoints'] is final score
         # it might also be used for points leagues instead of category leagues
-        if 'cumulativeScore' not in self.data['home'].keys():
+        if 'cumulativeScore' not in data['home'].keys():
             return 'Matchup(%s, %s)' % (self.home_team, self.away_team, )
         else:
             return 'Matchup(%s %s - %s %s)' % (self.home_team,
@@ -18,29 +17,29 @@ class Matchup(object):
                                                str(round(self.away_team_live_score, 1)),
                                                self.away_team)
 
-    def _fetch_matchup_info(self):
+    def _fetch_matchup_info(self, data):
         '''Fetch info for matchup'''
-        self.home_team = self.data['home']['teamId']
+        self.home_team = data['home']['teamId']
         self.home_team_live_score = None
-        self.home_final_score = self.data['home']['totalPoints']
-        self.away_team = self.data['away']['teamId']
+        self.home_final_score = data['home']['totalPoints']
+        self.away_team = data['away']['teamId']
         self.away_team_live_score = None
-        self.away_final_score = self.data['away']['totalPoints']
-        self.winner = self.data['winner']
+        self.away_final_score = data['away']['totalPoints']
+        self.winner = data['winner']
         self.home_team_cats = None
         self.away_team_cats = None
         
         # if stats are available
-        if 'cumulativeScore' in self.data['home'].keys():
+        if 'cumulativeScore' in data['home'].keys():
 
-            self.home_team_live_score = (self.data['home']['cumulativeScore']['wins'] +
-                                         self.data['home']['cumulativeScore']['ties']/2)
-            self.away_team_live_score = (self.data['away']['cumulativeScore']['wins'] +
-                                         self.data['away']['cumulativeScore']['ties']/2)
+            self.home_team_live_score = (data['home']['cumulativeScore']['wins'] +
+                                         data['home']['cumulativeScore']['ties']/2)
+            self.away_team_live_score = (data['away']['cumulativeScore']['wins'] +
+                                         data['away']['cumulativeScore']['ties']/2)
             
-            self.home_team_cats = { STATS_MAP[i]: {'score': self.data['home']['cumulativeScore']['scoreByStat'][i]['score'],
-                                                   'result': self.data['home']['cumulativeScore']['scoreByStat'][i]['result']} for i in self.data['home']['cumulativeScore']['scoreByStat'].keys()}
+            self.home_team_cats = { STATS_MAP[i]: {'score': data['home']['cumulativeScore']['scoreByStat'][i]['score'],
+                                                   'result': data['home']['cumulativeScore']['scoreByStat'][i]['result']} for i in data['home']['cumulativeScore']['scoreByStat'].keys()}
             
-            self.away_team_cats = { STATS_MAP[i]: {'score': self.data['away']['cumulativeScore']['scoreByStat'][i]['score'],
-                                                   'result': self.data['away']['cumulativeScore']['scoreByStat'][i]['result']} for i in self.data['away']['cumulativeScore']['scoreByStat'].keys()}
+            self.away_team_cats = { STATS_MAP[i]: {'score': data['away']['cumulativeScore']['scoreByStat'][i]['score'],
+                                                   'result': data['away']['cumulativeScore']['scoreByStat'][i]['result']} for i in data['away']['cumulativeScore']['scoreByStat'].keys()}
         
