c2c30c7046bbbf6a817dd326c2f4e1f927507600
Christian <cwendt94@vt.edu>
2020-09-08 17:48:37 -0700

    fix integration test import overlap

--- tests/espn_requests/test_espn_requests.py
+++ tests/espn_requests/test_espn_requests.py
@@ -7,7 +7,7 @@ class EspnRequestsTest(TestCase):
 
     @requests_mock.Mocker()
     @mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_authentication_api_fail(self, mock_request, mock_stdout, mock_fetch_league):
+    def test_authentication_api_fail(self, mock_request, mock_stdout):
         url_api_key = 'https://registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/api-key?langPref=en-US'
         mock_request.post(url_api_key, status_code=400)
         request = EspnFantasyRequests(sport='nfl', league_id=1234, year=2019)
@@ -16,7 +16,7 @@ class EspnRequestsTest(TestCase):
     
     @requests_mock.Mocker()
     @mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_authentication_login_fail(self, mock_request, mock_stdout, mock_fetch_league):
+    def test_authentication_login_fail(self, mock_request, mock_stdout):
         url_api_key = 'https://registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/api-key?langPref=en-US'
         url_login = 'https://ha.registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/guest/login?langPref=en-US'
         mock_request.post(url_api_key,  headers={'api-key':'None'}, status_code=200)
@@ -28,7 +28,7 @@ class EspnRequestsTest(TestCase):
     
     @requests_mock.Mocker()
     @mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_authentication_login_error(self, mock_request, mock_stdout, mock_fetch_league):
+    def test_authentication_login_error(self, mock_request, mock_stdout):
         url_api_key = 'https://registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/api-key?langPref=en-US'
         url_login = 'https://ha.registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/guest/login?langPref=en-US'
         mock_request.post(url_api_key,  headers={'api-key':'None'}, status_code=200)
@@ -39,7 +39,7 @@ class EspnRequestsTest(TestCase):
         self.assertEqual(mock_stdout.getvalue(), 'Authentication unsuccessful - error:{}\nRetry the authentication or continuing without private league access\n')
     
     @requests_mock.Mocker()
-    def test_authentication_pass(self, mock_request, mock_fetch_league):
+    def test_authentication_pass(self, mock_request):
         url_api_key = 'https://registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/api-key?langPref=en-US'
         url_login = 'https://ha.registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/guest/login?langPref=en-US'
         mock_request.post(url_api_key,  headers={'api-key':'None'}, status_code=200)
