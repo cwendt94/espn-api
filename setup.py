c9288e1989b921c7ef03073bf9dc118ea7202bf1
Christian Wendt <cwendt94@vt.edu>
2020-09-15 20:24:15 -0700

    Package bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='espn_api',
     packages=find_packages(),
-    version='0.5.1',
+    version='0.6.0',
     author='Christian Wendt',
     description='ESPN API',
     install_requires=['requests>=2.0.0,<3.0.0'],
