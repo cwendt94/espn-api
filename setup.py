bcab24630d05238049898cc8ca39b985c7db976c
Christian <cwendt94@vt.edu>
2021-08-23 08:39:23 -0700

    Remove Panda Dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='espn_api',
     packages=find_packages(),
-    version='0.17.0',
+    version='0.18.0',
     author='Christian Wendt',
     description='ESPN API',
-    install_requires=['requests>=2.0.0,<3.0.0', 'pandas>=0.11.0,<=0.25.3', 'numpy>=1.15,<=1.17.5'],
+    install_requires=['requests>=2.0.0,<3.0.0'],
     setup_requires=['nose>=1.0'],
     test_suite='nose.collector',
     tests_require=['nose', 'requests_mock', 'coverage'],
