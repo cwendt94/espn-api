afe9d969981f6833f47c6c9464b9d65a88652b45
Christian Wendt <cwendt94@vt.edu>
2022-11-22 17:54:42 +0000

    Football Account for Away team being on bye

--- espn_api/football/matchup.py
+++ espn_api/football/matchup.py
@@ -1,22 +1,19 @@
 class Matchup(object):
     '''Creates Matchup instance'''
     def __init__(self, data):
-        self.data = data
-        self._fetch_matchup_info()
+        self.matchup_type = data.get('playoffTierType', 'NONE')
+        self.is_playoff = self.matchup_type != 'NONE'
+        (self.home_team, self.home_score) = self._fetch_matchup_info(data, 'home')
+        (self.away_team, self.away_score) = self._fetch_matchup_info(data, 'away')
 
     def __repr__(self):
         return f'Matchup({self.home_team}, {self.away_team})'
 
-    def _fetch_matchup_info(self):
+    def _fetch_matchup_info(self, data, team):
         '''Fetch info for matchup'''
-        self.home_team = self.data['home']['teamId']
-        self.home_score = self.data['home']['totalPoints']
-        self.matchup_type = self.data.get('playoffTierType', 'NONE')
-        self.is_playoff = self.matchup_type != 'NONE'
+        if team not in data:
+            return (0, 0)
+        team_id = data[team]['teamId']
+        team_score = data[team]['totalPoints']
 
-        # For Leagues with bye weeks
-        self.away_team = 0
-        self.away_score = 0
-        if 'away' in self.data:
-            self.away_team = self.data['away']['teamId']
-            self.away_score = self.data['away']['totalPoints']
+        return (team_id, team_score)
