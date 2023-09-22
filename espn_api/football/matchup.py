0a8d0d43dd0c9e98806ce0bc896ac89562978379
Sean Kane <spkane31@gmail.com>
2023-09-21 21:29:50 -0400

    removing type annotation

--- espn_api/football/matchup.py
+++ espn_api/football/matchup.py
@@ -5,15 +5,15 @@ class Matchup(object):
     def __init__(self, data):
         self.matchup_type = data.get('playoffTierType', 'NONE')
         self.is_playoff = self.matchup_type != 'NONE'
-        (self._home_team_id, self.home_score) = self._fetch_matchup_info(data, 'home')
-        (self._away_team_id, self.away_score) = self._fetch_matchup_info(data, 'away')
+        (self._home_team_id: int, self.home_score: float) = self._fetch_matchup_info(data, 'home')
+        (self._away_team_id: int, self.away_score: float) = self._fetch_matchup_info(data, 'away')
         self.home_team: Team
         self.away_team: Team
 
     def __repr__(self):
         return f'Matchup({self.home_team}, {self.away_team})'
 
-    def _fetch_matchup_info(self, data, team) -> tuple[int, float]:
+    def _fetch_matchup_info(self, data, team):
         '''Fetch info for matchup'''
         if team not in data:
             return (0, 0)
