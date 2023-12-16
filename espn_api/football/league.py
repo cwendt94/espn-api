63e390d91f84c21b1507425015ec889c210dbf39
DesiPilla <desi.m.pilla@gmail.com>
2023-12-15 23:49:00 -0500

    feat: Add method to calculate historical standings for any week
    Subject to the league's specific tiebreaker rules

--- espn_api/football/league.py
+++ espn_api/football/league.py
@@ -1,7 +1,9 @@
-import datetime
-import time
 import json
-from typing import List, Tuple, Union
+from itertools import combinations
+from typing import Callable, Dict, List, Tuple, Union
+
+import pandas as pd
+
 
 from ..base_league import BaseLeague
 from .team import Team
@@ -14,6 +16,7 @@ from .activity import Activity
 from .settings import Settings
 from .utils import power_points, two_step_dominance
 from .constant import POSITION_MAP, ACTIVITY_MAP
+
 
 class League(BaseLeague):
     '''Creates a League instance for Public/Private ESPN league'''
@@ -115,6 +118,108 @@ class League(BaseLeague):
     def standings(self) -> List[Team]:
         standings = sorted(self.teams, key=lambda x: x.final_standing if x.final_standing != 0 else x.standing, reverse=False)
         return standings
+
+    def standings_weekly(self, week: int) -> pd.DataFrame:
+        """This is the main function to get the standings for a given week.
+
+        It controls the tiebreaker hierarchy and calls the recursive League()._sort_standings_df function.
+        First, the division winners must be determined. Then, the rest of the teams are sorted.
+
+        The standard tiebreaker hierarchy is:
+            1. Head-to-head record among the tied teams
+            2. Total points scored for the season
+            3. Division record (if all tied teams are in the same division)
+            4. Total points scored against for the season
+            5. Coin flip
+
+        Args:
+            week (int): Week to get the standings for
+
+        Returns:
+            pd.DataFrame: Sorted standings DataFrame
+        """
+        # Build the standings DataFrame for the given week
+        standings_df = pd.DataFrame()
+        standings_df["team_id"] = [team.team_id for team in self.teams]
+        standings_df["team_name"] = [team.team_name for team in self.teams]
+        standings_df["division_id"] = [team.division_id for team in self.teams]
+        standings_df["division_name"] = [team.division_name for team in self.teams]
+        standings_df["wins"] = [
+            sum([1 for outcome in team.outcomes[:week] if outcome == "W"])
+            for team in self.teams
+        ]
+        standings_df["ties"] = [
+            sum([1 for outcome in team.outcomes[:week] if outcome == "T"])
+            for team in self.teams
+        ]
+        standings_df["losses"] = [
+            sum([1 for outcome in team.outcomes[:week] if outcome == "L"])
+            for team in self.teams
+        ]
+        standings_df["points_for"] = [sum(team.scores[:week]) for team in self.teams]
+        standings_df["points_against"] = [
+            sum([team.schedule[w].scores[w] for w in range(week)])
+            for team in self.teams
+        ]
+        standings_df["win_pct"] = (
+            standings_df["wins"] + standings_df["ties"] / 2
+        ) / standings_df[["wins", "losses", "ties"]].sum(axis=1)
+        standings_df.set_index("team_id", inplace=True)
+
+        if self.settings.playoff_seed_tie_rule == "TOTAL_POINTS_SCORED":
+            tiebreaker_hierarchy = [
+                (self._sort_by_win_pct, "win_pct"),
+                (self._sort_by_points_for, "points_for"),
+                (self._sort_by_head_to_head, "h2h_wins"),
+                (self._sort_by_division_record, "division_record"),
+                (self._sort_by_points_against, "points_against"),
+                (self._sort_by_coin_flip, "coin_flip"),
+            ]
+        elif self.settings.playoff_seed_tie_rule == "H2H_RECORD":
+            tiebreaker_hierarchy = [
+                (self._sort_by_win_pct, "win_pct"),
+                (self._sort_by_head_to_head, "h2h_wins"),
+                (self._sort_by_points_for, "points_for"),
+                (self._sort_by_division_record, "division_record"),
+                (self._sort_by_points_against, "points_against"),
+                (self._sort_by_coin_flip, "coin_flip"),
+            ]
+        else:
+            raise ValueError(
+                "Unkown tiebreaker_method: Must be either 'TOTAL_POINTS_SCORED' or 'H2H_RECORD'"
+            )
+
+        # First assign the division winners
+        division_winners = pd.DataFrame()
+        for division_id in standings_df.division_id.unique():
+            division_standings = standings_df[(standings_df.division_id == division_id)]
+            division_winner = self._sort_standings_df(
+                division_standings, tiebreaker_hierarchy
+            ).iloc[0]
+            division_winners = pd.concat(
+                [division_winners, division_winner.to_frame().T]
+            )
+
+        # Sort the division winners
+        sorted_division_winners = self._sort_standings_df(
+            division_winners, tiebreaker_hierarchy
+        )
+
+        # Then sort the rest of the teams
+        rest_of_field = standings_df.loc[
+            ~standings_df.index.isin(division_winners.index)
+        ]
+        sorted_rest_of_field = self._sort_standings_df(
+            rest_of_field, tiebreaker_hierarchy
+        )
+
+        # Combine all teams
+        sorted_standings = pd.concat(
+            [sorted_division_winners, sorted_rest_of_field],
+            axis=0,
+        )
+
+        return sorted_standings
 
     def top_scorer(self) -> Team:
         most_pf = sorted(self.teams, key=lambda x: x.points_for, reverse=True)
@@ -309,3 +414,167 @@ class League(BaseLeague):
                 messages.append(msg)
         return messages
 
+    def _build_division_record_dict(self) -> Dict:
+        """Create a DataFrame with each team's divisional record."""
+        # Get the list of teams
+        team_ids = [team.team_id for team in self.teams]
+
+        # Create a dictionary with each team's divisional record
+        div_outcomes = {
+            team.team_id: {"wins": 0, "divisional_games": 0} for team in self.teams
+        }
+
+        # Loop through each team's schedule and outcomes and build the dictionary
+        for team in self.teams:
+            for opp, outcome in zip(team.schedule[:week], team.outcomes[:week]):
+                if team.division_id == opp.division_id:
+                    if outcome == "W":
+                        div_outcomes[team.team_id]["wins"] += 1
+                    if outcome == "T":
+                        div_outcomes[team.team_id]["wins"] += 0.5
+
+                    div_outcomes[team.team_id]["divisional_games"] += 1
+
+        # Calculate the divisional record
+        div_record = {
+            team_id: (
+                div_outcomes[team_id]["wins"]
+                / max(div_outcomes[team_id]["divisional_games"], 1)
+            )
+            for team_id in team_ids
+        }
+
+        return div_record
+
+    def _build_h2h_df(self) -> pd.DataFrame:
+        """Create a dataframe that contains the head-to-head victories between each team in the league"""
+        # Get the list of teams
+        team_ids = [team.team_id for team in self.teams]
+
+        # Initialize the DataFrame
+        h2h_df = pd.DataFrame(index=team_ids, columns=team_ids).replace(np.nan, 0)
+
+        # Loop through each team
+        for team in self.teams:
+            # Get the team's schedule
+            schedule = team.schedule
+            outcomes = team.outcomes
+
+            # Loop through each week
+            for week, opp in enumerate(schedule):
+                # Update the DataFrame
+                if outcomes[week] == "W":
+                    h2h_df.loc[team.team_id, opp.team_id] += 1
+
+                if outcomes[week] == "T":
+                    h2h_df.loc[team.team_id, opp.team_id] += 0.5
+
+        return h2h_df
+
+    def _sort_by_win_pct(self, standings_df: pd.DataFrame) -> pd.DataFrame:
+        """Take a DataFrame of standings and sort it using the TOTAL_POINTS_SCORED tiebreaker"""
+        return standings_df.sort_values(by="win_pct", ascending=False)
+
+    def _sort_by_points_for(self, standings_df: pd.DataFrame) -> pd.DataFrame:
+        """Take a DataFrame of standings and sort it using the TOTAL_POINTS_SCORED tiebreaker"""
+        return standings_df.sort_values(by="points_for", ascending=False)
+
+    def _sort_by_division_record(self, standings_df: pd.DataFrame) -> pd.DataFrame:
+        division_records = build_division_record_df(self)
+        standings_df["division_record"] = standings_df.index.map(division_records)
+        return standings_df.sort_values(by="division_record", ascending=False)
+
+    def _sort_by_points_against(self, standings_df: pd.DataFrame) -> pd.DataFrame:
+        """Take a DataFrame of standings and sort it using the 3rd level tiebreaker"""
+        return standings_df.sort_values(by="points_against", ascending=True)
+
+    def _sort_by_coin_flip(self, standings_df: pd.DataFrame) -> pd.DataFrame:
+        standings_df["coin_flip"] = np.random.rand(len(standings_df))
+        return standings_df.sort_values(by="coin_flip", ascending=False)
+
+    def _sort_by_head_to_head(
+        self,
+        standings_df: pd.DataFrame,
+    ) -> pd.DataFrame:
+        """Take a DataFrame of standings and sort it using the H2H_RECORD tiebreaker"""
+        # If there is only one team, return the dataframe as-is
+        if len(standings_df) == 1:
+            return standings_df
+
+        # Filter the H2H DataFrame to only include the teams in question
+        team_ids = standings_df.index.tolist()
+        h2h_df = self._build_h2h_df().loc[team_ids, team_ids]
+
+        # If there are only two teams, sort descending by H2H wins
+        if len(h2h_df) == 2:
+            standings_df["h2h_wins"] = h2h_df.sum(axis=1)
+            return standings_df.sort_values(by="h2h_wins", ascending=False)
+
+        # If there are more than two teams...
+        if len(h2h_df) > 2:
+            # Check if the teams have all played each other an equal number of times
+            matchup_combos = list(combinations(team_ids, 2))
+            matchup_counts = [
+                h2h_df.loc[t1, t2] + h2h_df.loc[t2, t1] for t1, t2 in matchup_combos
+            ]
+            if len(set(matchup_counts)) == 1:
+                # All teams have played each other an equal number of times
+                # Sort the teams by total H2H wins against each other
+                standings_df["h2h_wins"] = h2h_df.sum(axis=1)
+                return standings_df.sort_values(by="h2h_wins", ascending=False)
+            else:
+                # All teams have not played each other an equal number of times
+                # This tiebreaker is invalid
+                standings_df["h2h_wins"] = 0
+                return standings_df
+
+    def _sort_standings_df(
+        self,
+        standings_df: pd.DataFrame,
+        tiebreaker_hierarchy: List[Tuple[Callable, str]],
+    ) -> pd.DataFrame:
+        """This recursive function sorts a standings DataFrame by the tiebreaker hierarchy.
+        It iterates through each tiebreaker, sorting any remaning ties by the next tiebreaker.
+
+        Args:
+            standings_df (pd.DataFrame): Standings DataFrame to sort
+            tiebreaker_hierarchy (List[Tuple[Callable, str]]): List of tiebreaker functions and columns to sort by
+
+        Returns:
+            pd.DataFrame: Sorted standings DataFrame
+        """
+        # If there are no more tiebreakers, return the standings DataFrame as-is
+        if not tiebreaker_hierarchy:
+            return standings_df
+
+        # If there is only one team to sort, return the standings DataFrame as-is
+        if len(standings_df) == 1:
+            return standings_df
+
+        # Get the tiebreaker function and column name to group by
+        tiebreaker_function = tiebreaker_hierarchy[0][0]
+        tiebreaker_col = tiebreaker_hierarchy[0][1]
+
+        # Apply the tiebreaker function to the standings DataFrame
+        standings_df = tiebreaker_function(standings_df)
+
+        # Loop through each remaining unique tiebreaker value to see if ties remain
+        sorted_standings = pd.DataFrame()
+        for val in sorted(standings_df[tiebreaker_col].unique(), reverse=True):
+            standings_subset = standings_df[standings_df[tiebreaker_col] == val]
+
+            # Sort all remaining teams with the next tiebreaker
+            sorted_subset = self._sort_standings_df(
+                standings_subset,
+                tiebreaker_hierarchy[1:],
+            )
+
+            # Append the sorted subset to the final sorted standings DataFrame
+            sorted_standings = pd.concat(
+                [
+                    sorted_standings,
+                    sorted_subset,
+                ]
+            )
+
+        return sorted_standings
