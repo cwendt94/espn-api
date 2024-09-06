bf3bbf4b415b2fc86c1981b6f1f37636aba35396
martincham <martinchamberlin12@gmail.com>
2024-09-06 17:42:33 -0400

    undo linting

--- tests/basketball/integration/test_league.py
+++ tests/basketball/integration/test_league.py
@@ -10,7 +10,7 @@ class LeagueTest(TestCase):
     def test_league_init(self):
         self.assertEqual(self.league.scoringPeriodId, 178)
         player = self.league.teams[0].roster[0]
-        self.assertEqual(player.schedule["2"]["team"], "BKN")
+        self.assertEqual(player.schedule['2']['team'], 'BKN')
         self.assertEqual(player.total_points, 3583.0)
         self.assertEqual(player.avg_points, 45.35)
 
@@ -23,10 +23,10 @@ class LeagueTest(TestCase):
     def test_league_draft(self):
         draft = self.league.draft
 
-        self.assertEqual(draft[1].playerName, "LeBron James")
+        self.assertEqual(draft[1].playerName, 'LeBron James')
         self.assertEqual(draft[1].round_num, 1)
         self.assertEqual(draft[2].round_pick, 3)
-        self.assertEqual(draft[2].team.team_name, "Denver  Nuggets ")
+        self.assertEqual(draft[2].team.team_name, 'Denver  Nuggets ')
 
     def test_league_free_agents(self):
         free_agents = self.league.free_agents()
@@ -38,22 +38,22 @@ class LeagueTest(TestCase):
 
         player = self.league.player_info(playerId=player_id)
 
-        self.assertEqual(player.__repr__(), "Player(Andre Drummond)")
-        self.assertEqual(player.schedule["2"]["team"], "BKN")
-        self.assertEqual(player.stats["2"]["team"], "BKN")
-        self.assertEqual(player.stats["2"]["total"]["PTS"], 24.0)
+        self.assertEqual(player.__repr__(), 'Player(Andre Drummond)')
+        self.assertEqual(player.schedule['2']['team'], 'BKN')
+        self.assertEqual(player.stats['2']['team'], 'BKN')
+        self.assertEqual(player.stats['2']['total']['PTS'], 24.0)
         self.assertEqual(
             player.nine_cat_averages,
             {
-                "PTS": 17.3,
-                "BLK": 1.7,
-                "STL": 1.7,
-                "AST": 1.4,
-                "REB": 15.6,
-                "TO": 2.2,
-                "3PM": 0.1,
-                "FG%": 0.533,
-                "FT%": 0.59,
+                'PTS': 17.3,
+                'BLK': 1.7,
+                'STL': 1.7,
+                'AST': 1.4,
+                'REB': 15.6,
+                'TO': 2.2,
+                '3PM': 0.1,
+                'FG%': 0.533,
+                'FT%': 0.59,
             },
         )
 
@@ -80,11 +80,11 @@ class LeagueTest(TestCase):
         score = league.box_scores(matchup_period=3, scoring_period=21)
 
         self.assertEqual(
-            score[0].__repr__(), "Box Score(Team(Team McWilliams) at Team(Team Wendt))"
+            score[0].__repr__(), 'Box Score(Team(Team McWilliams) at Team(Team Wendt))'
         )
-        self.assertEqual(score[0].away_lineup[0].name, "Stephen Curry")
+        self.assertEqual(score[0].away_lineup[0].name, 'Stephen Curry')
         # comment for now until matchup week is over
-        self.assertEqual(score[0].away_stats["PTS"], {"value": 733.0, "result": "WIN"})
+        self.assertEqual(score[0].away_stats['PTS'], {'value': 733.0, 'result': 'WIN'})
 
     def test_past_league(self):
         league = League(411647, 2017)
