257d4ed88a5feca9162b696388dfc454f14416a4
Sean Kane <spkane31@gmail.com>
2025-12-15 16:38:12 -0700

    addressing comments

--- espn_api/football/box_player.py
+++ espn_api/football/box_player.py
@@ -4,48 +4,57 @@ from datetime import datetime, timedelta
 
 
 class BoxPlayer(Player):
-    '''player with extra data from a matchup'''
+    """player with extra data from a matchup"""
+
     def __init__(self, data, pro_schedule, positional_rankings, week, year):
         super(BoxPlayer, self).__init__(data, year)
-        self.slot_position = 'FA'
-        self.pro_opponent = "None" # professional team playing against
-        self.pro_pos_rank = 0 # rank of professional team against player position
-        self.game_played = 100 # 0-100 for percent of game played
+        self.slot_position = "FA"
+        self.pro_opponent = "None"  # professional team playing against
+        self.pro_pos_rank = 0  # rank of professional team against player position
+        self.game_played = 100  # 0-100 for percent of game played
         self.on_bye_week = False
 
-        if 'lineupSlotId' in data:
-            self.slot_position = POSITION_MAP[data['lineupSlotId']]
+        if "lineupSlotId" in data:
+            self.slot_position = POSITION_MAP[data["lineupSlotId"]]
 
-        player = data['playerPoolEntry']['player'] if 'playerPoolEntry' in data else data['player']
-        if player['proTeamId'] in pro_schedule:
-            (opp_id, date) = pro_schedule[player['proTeamId']]
-            self.game_date = datetime.fromtimestamp(date/1000.0)
-            self.game_played = 100 if datetime.now() > self.game_date + timedelta(hours=3) else 0
-            posId = str(player['defaultPositionId'])
+        player = (
+            data["playerPoolEntry"]["player"]
+            if "playerPoolEntry" in data
+            else data["player"]
+        )
+        if player["proTeamId"] in pro_schedule:
+            (opp_id, date) = pro_schedule[player["proTeamId"]]
+            self.game_date = datetime.fromtimestamp(date / 1000.0)
+            self.game_played = (
+                100 if datetime.now() > self.game_date + timedelta(hours=3) else 0
+            )
+            posId = str(player["defaultPositionId"])
             if posId in positional_rankings:
                 self.pro_opponent = PRO_TEAM_MAP[opp_id]
-                self.pro_pos_rank = positional_rankings[posId][str(opp_id)] if str(opp_id) in positional_rankings[posId] else 0
-        else: # bye week
+                self.pro_pos_rank = (
+                    positional_rankings[posId][str(opp_id)]
+                    if str(opp_id) in positional_rankings[posId]
+                    else 0
+                )
+        else:  # bye week
             self.on_bye_week = True
 
         stats = self.stats.get(week, {})
-        self.points = stats.get('points', 0)
-        self.points_breakdown = stats.get('breakdown', 0)
-        self.projected_points = stats.get('projected_points', 0)
-        self.projected_breakdown = stats.get('projected_breakdown', 0)
+        self.points = stats.get("points", 0)
+        self.points_breakdown = stats.get("breakdown", 0)
+        self.projected_points = stats.get("projected_points", 0)
+        self.projected_breakdown = stats.get("projected_breakdown", 0)
 
         # Backup projected_points extraction from raw data if not available from stats
         if self.projected_points == 0:
-            try:
-                player_pool_entry = data.get('playerPoolEntry', {})
-                player_stats = player_pool_entry.get('stats', [])
-                if len(player_stats) > 1:
-                    projected_stat = player_stats[1]
-                    backup_projected = projected_stat.get('appliedTotal', 0)
-                    if backup_projected > 0:
-                        self.projected_points = backup_projected
-            except (KeyError, IndexError, AttributeError):
-                pass
+            player_pool_entry = data.get("playerPoolEntry", {})
+            player_stats = player_pool_entry.get("stats", [])
+            for stats in player_stats:
+                if stats.get("seasonId") != year or stats.get("statSplitTypeId") == 2:
+                    continue
+                if stats.get("statSourceId") == 0:
+                    self.projected_points = stats.get("appliedTotal", 0)
+                    break
 
     def __repr__(self):
-        return f'Player({self.name}, points:{self.points}, projected:{self.projected_points})'
+        return f"Player({self.name}, points:{self.points}, projected:{self.projected_points})"
