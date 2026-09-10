25825284c48fe6524e134d163e4954df51163781
Quadram13 <marcus.du@gmail.com>
2026-09-10 14:24:22 +0800

    fix(basketball, football): handle transactions with missing items
    - Default items to [] for transaction types that carry no items (matches baseball change at commit bf18c45)

--- espn_api/football/transaction.py
+++ espn_api/football/transaction.py
@@ -9,7 +9,7 @@ class Transaction(object):
             self.date = data.get('proposedDate')
         self.bid_amount = data.get('bidAmount')
         self.items = []
-        for item in data['items']:
+        for item in data.get('items', []):
             self.items.append(TransactionItem(item, player_map))
 
     def __repr__(self):
