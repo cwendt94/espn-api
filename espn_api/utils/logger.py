8a5672301000c1c3822999f35cc6ae9aa0bf2cf0
Quadram13 <marcus.du@gmail.com>
2026-09-17 15:46:44 +0800

    chore: add type annotations to logger module
    mypy and tests come back clean

--- espn_api/utils/logger.py
+++ espn_api/utils/logger.py
@@ -1,12 +1,13 @@
 import logging
 import sys
 import json
+from typing import Any, Dict, Optional
 
 
 class Logger(object):
-    def __init__(self, name: str, debug=False):
+    def __init__(self, name: str, debug: bool = False) -> None:
         level = logging.DEBUG if debug else logging.INFO
-        self.logging = logging.getLogger(name)
+        self.logging: logging.Logger = logging.getLogger(name)
 
         # if logger already exists don't add handlers
         if len(self.logging.handlers):
@@ -22,8 +23,12 @@ class Logger(object):
         self.logging.setLevel(level)
 
     def log_request(
-        self, endpoint: str, response: dict, params: dict = None, headers: dict = None
-    ):
+        self,
+        endpoint: str,
+        response: Any,
+        params: Optional[Dict[str, Any]] = None,
+        headers: Optional[Dict[str, Any]] = None,
+    ) -> None:
         log = f"ESPN API Request: url: {endpoint} params: {params} headers: {headers} \nESPN API Response: {json.dumps(response)}"
         self.logging.debug(log)
 
